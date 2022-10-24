3d2ee1cd21428883c19620de73c0358a65f70280
kevstevie <jianna442@gmail.com>
2022-10-24 15:30:42 +0900

    fix: class Node, map

--- src/BOJ1890.java
+++ src/BOJ1890.java
@@ -1,30 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 class BOJ1890 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] map = new int[n][n];
+        map = new int[n][n];
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        
-    }
-}
-class Node{
-    int x;
-    int y;
-    int jump;
+        Stack<Node> dfs = new Stack<>();
 
-    public Node(int x, int y, int jump) {
-        this.x = x;
-        this.y = y;
-        this.jump = jump;
+    }
+    static int[][] map;
+
+
+    class Node{
+        int x;
+        int y;
+        int jump;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+            jump = map[x][y];
+        }
     }
 }
