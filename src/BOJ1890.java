73de92b96367fbd48d95aeb1adc24d01d986f898
kevstevie <jianna442@gmail.com>
2022-10-24 16:19:13 +0900

    fix : dfs to dp

--- src/BOJ1890.java
+++ src/BOJ1890.java
@@ -1,54 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
-class BOJ1890 {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        map = new int[n][n];
+        int[][] map = new int[n][n];
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        Queue<Node> bfs = new LinkedList<>();
-
-        long count = 0;
-
-        bfs.offer(new Node(0, 0));
-        while (!bfs.isEmpty()) {
-            Node now = bfs.poll();
-            if (now.x == n - 1 && now.y == n - 1){
-                count++;
-                continue;
-            }
-            if (now.jump == 0 ) continue;
-            if (now.x + now.jump < n) {
-                bfs.offer(new Node(now.x + now.jump, now.y));
-            }
-            if (now.y + now.jump <n){
-                bfs.offer(new Node(now.x, now.y + now.jump));
+        long[][] dp = new long[n][n];
+        dp[0][0] = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int x = i + map[i][j];
+                int y = j + map[i][j];
+                if (map[i][j] == 0) continue;
+                if (dp[i][j] == 0) continue;
+                if ( x < n){
+                    dp[x][j]++;
+                }
+                if ( y < n){
+                    dp[i][y]++;
+                }
             }
         }
-        System.out.println(count);
-
-    }
-    static int[][] map;
-
-    static class Node{
-        int x;
-        int y;
-        int jump;
-
-        public Node(int x, int y) {
-            this.x = x;
-            this.y = y;
-            jump = map[x][y];
-        }
+        System.out.println(dp[n-1][n-1]);
     }
 }
