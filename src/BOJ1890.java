d1fa0395b171c3754c5b9ca0019d2d9b0e5aba6f
kevstevie <jianna442@gmail.com>
2022-10-24 15:39:58 +0900

    feat: dfs

--- src/BOJ1890.java
+++ src/BOJ1890.java
@@ -17,11 +17,28 @@ class BOJ1890 {
         }
         Stack<Node> dfs = new Stack<>();
 
+        long count = 0;
+
+        dfs.push(new Node(0, 0));
+        while (!dfs.isEmpty()) {
+            Node now = dfs.pop();
+            if (now.jump == 0 ) continue;
+            if (now.x == n - 1 && now.y == n - 1){
+                count++;
+                continue;
+            }
+            if (now.x + now.jump < n) {
+                dfs.push(new Node(now.x + now.jump, now.y));
+            } else if (now.y + now.jump <n){
+                dfs.push(new Node(now.x, now.y + now.jump));
+            }
+        }
+        System.out.println(count);
+
     }
     static int[][] map;
 
-
-    class Node{
+    static class Node{
         int x;
         int y;
         int jump;
